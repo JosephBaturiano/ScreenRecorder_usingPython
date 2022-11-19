cb374f36fbd813072d5c87bcb53246325bc3b6f7
Joseph Baturiano <josephbaturiano3@gmail.com>
2022-11-19 20:51:04 +0700

    Install pywin32 and fix the height and weight

--- ScreenRecorder.py
+++ ScreenRecorder.py
@@ -1,9 +1,15 @@
 from PIL import ImageGrab
 import numpy as np
 import cv2
+import win32api import GetSystemMetrics
+
+width = GetSystemMetrics(0)
+height = GetSystemMetrics(1)
+
+print(width, height)
 
 while True:
-    img = ImageGrab.grab(bbox=(0, 0, 1280, 720,))
+    img = ImageGrab.grab(bbox=(0, 0, width, height,))
     img_np = np.array(img)
     img_final = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
     cv2.imshow('Screen Capture', img_final)
