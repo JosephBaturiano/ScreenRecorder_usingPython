030a548ac27f7c2ee76499d07a11f773334cbbd9
Joseph Baturiano <josephbaturiano3@gmail.com>
2022-11-19 20:45:20 +0700

    Fixing the color in recording

--- ScreenRecorder.py
+++ ScreenRecorder.py
@@ -5,5 +5,6 @@ import cv2
 while True:
     img = ImageGrab.grab(bbox=(0, 0, 1280, 720,))
     img_np = np.array(img)
-    cv2.imshow('Screen Capture', img_np)
+    img_final = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+    cv2.imshow('Screen Capture', img_final)
     cv2.waitKey(10)
